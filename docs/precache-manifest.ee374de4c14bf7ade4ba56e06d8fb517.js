b9a41c9ae04ed3d961f577162050361f8156118d
lcolok(Home) <425311101@qq.com>
2019-05-04 00:46:38 +0800

    build static pages

---
+++ docs/precache-manifest.ee374de4c14bf7ade4ba56e06d8fb517.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ea2915ba880ad2cd94d",
+    "url": "/js/demo-legacy.93abb134.js"
+  },
+  {
+    "revision": "1da4bbb95a62ee193a9a",
+    "url": "/js/chunk-vendors-legacy.93548a27.js"
+  },
+  {
+    "revision": "4e70eaafea7fb5d226dd",
+    "url": "/js/app-legacy.9f63b4c1.js"
+  },
+  {
+    "revision": "c61670247df54355347c8b2c73c12fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "4e70eaafea7fb5d226dd",
+    "url": "/css/app.36b715e9.css"
+  }
+];
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  publicPath: isProd ? "/vue-argon-dashboard/" : "",
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
@@ -19,7 +18,7 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: '#172b4d'
   },
   css: {
-    // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
-  }
+    sourceMap: true
+  },
+  productionSourceMap: false
 };
